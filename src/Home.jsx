ec6958f2303c094963e2a436de6e07aedb895d7e
Marlon Ferreira <ferreira.marlon@live.com>
2024-07-31 00:28:24 -0300

    feat: create routes

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Register from "./pages/Register/Register";
+import Login from "./pages/Login/Login";
+import UserList from "./pages/UserList/UserList";
+
 const Home = () => {
-    return <h1>Home</h1>;
+   return (
+      <>
+         <BrowserRouter>
+            <Routes>
+               <Route path="/" element={<Login />} />
+               <Route path="/register" element={<Register />} />
+               <Route path="/userlist" element={<UserList />} />
+            </Routes>
+         </BrowserRouter>
+      </>
+   );
 };
 
 export default Home;
